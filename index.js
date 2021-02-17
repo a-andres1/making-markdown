2e3046b57a9a41934a4d80b4c4c9313830b0fe18
alyssa andres <alyssaandres1@gmail.com>
2021-02-17 15:46:17 -0500

    attempting to write async function. want to move machines so uploading json files

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // TODO: Include packages needed for this application
 var inquirer = require("inquirer");
 var fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
+const renderLicenseLink = require("./utils/generateMarkdown");
+const renderLicenseBadge = require("./utils/generateMarkdown");
+const renderLicenseSection = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [  inquirer.prompt([
     {
         type: "input",
-        name: "fileName",
+        name: "filename",
         message:"What should we call your file?",
     },
     {
@@ -17,7 +21,7 @@ const questions = [  inquirer.prompt([
     {
         type: "input",
         name: "desctiption",
-        message:"Tell me about your project.",
+        message:"Tell us about your project.",
     },
     {
         type: "input",
@@ -60,6 +64,20 @@ const questions = [  inquirer.prompt([
         message:"What is the link to your deployed app?",
     },
     {
+        type: "list",
+        name: "license",
+        message:"What kind of license do you require?",
+        choices: [
+            'MIT',
+            'GPLv2',
+            'GPLv3',
+            'Apache',
+            'BDS 3-clause',
+            'BDS 2-clause',
+            'Unlicense',
+        ]
+    },
+    {
         type: "input",
         name: "github",
         message:"What is your GitHub username?",
@@ -70,42 +88,34 @@ const questions = [  inquirer.prompt([
         message:"What is your email?",
     },
 ]).then(function(data) {
-    console.log(data);
+    // console.log(data);
+    var licenseName = data.license;
+    console.log(licenseName);
+    // renderLicenseLink(licenseName);
+    // renderLicenseBadge(licenseName);
+    // renderLicenseSection(licenseName);
+    // $.ajax({
+    //     url: "application/vnd.github.v3+json",
+    //     method: "GET",
+    //     dataType: "json",
+    //     license: licenseName
+    // }).then(function (response){
+    // console.log(response)
+    // })
+    // try {await octokit.request('GET /licenses/{license}', {
+    //     license: licenseName
+    //   })} catch(error){
+    //     console.log("nope")
+    // }
+    
 })];
 
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return `
-    # ${data.title}
-
-    ## Description
-    ${data.desctiption}
-    
-    ## Technologies Used
-    ${data.tech}
-    
-    ## Install
-    ${data.install}
-    
-    ## Usage
-    ${data.usage}
-    
-    ## Credits
-    ${data.contributions}
-    ${data.resources}
-    
-    ## License Info
-    ${data.license} 
-    
-    ## Links
-    [Repo](${data.repo})
-    [Site](${data.deployed})
-    
-    ## Contact Info
-    [email me](mailto:${data.email})  
-    [find me on github](${data.github})`
+    // this should be the async functions for writing to the file. I'm not sure what to do with the fileName part yet... 
+    generateMarkdown(data)
     
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenseName) {
+  console.log(licenseName);
+  $.ajax({
+    url: "application/vnd.github.v3+json",
+    method: "GET",
+    dataType: "json",
+    license: licenseName
+}).then(function (response){
+console.log(response)
+})
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(licenseName) {
+  console.log(licenseName);
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenseName) {
+  console.log(licenseName);
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## Description
+  ${data.desctiption}
+  
+  ## Technologies Used
+  ${data.tech}
+  
+  ## Install
+  ${data.install}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Credits
+  ${data.contributions}
+  ${data.resources}
+  
+  ## License Info
+  ${data.license} 
+  
+  ## Links
+  [Repo](${data.repo})
+  [Site](${data.deployed})
+  
+  ## Contact Info
+  [email me](mailto:${data.email})  
+  [find me on github](${data.github})`;
 }
 
 module.exports = generateMarkdown;
+module.exports = renderLicenseBadge;
+module.exports = renderLicenseLink;
+module.exports = renderLicenseSection;
